d7aa3769f439309c5768bff86fffc85a3a81e895
Mikhail <mihail.zharavin65@gmail.com>
2023-01-12 17:59:06 +0200

    Made an adaptive for the ContentHome component

--- src/components/HomeContent/AboutMe/AboutMe.tsx
+++ src/components/HomeContent/AboutMe/AboutMe.tsx
@@ -3,17 +3,26 @@ import { HomeTitle } from "../HomeTitle/HomeTitle";
 import { DownCircleFilled } from "@ant-design/icons";
 import styles from "./aboutMe.module.scss";
 import MyPhoto from "../../../img/Home/Me.png";
+import { Avatar } from "antd";
+
 export const AboutMe: React.FC = () => {
   return (
-    <div className={styles.aboutMe}>
+    <div className={styles.aboutMeWrapper}>
       <div className="title">
         <HomeTitle title="about me" />
       </div>
       <div className={styles.content}>
         <div className={styles.boxDescription}>
-          <h1 className={styles.descriptionTitle}>
-            Hello, I’m Mikhail, Junior Front-end Developer.
-          </h1>
+          <div className={styles.boxDescriptionTitle}>
+            <Avatar
+              className={styles.boxDescriptionPhoto}
+              size={{ md: 100, sm: 80, xs: 60 }}
+              src={MyPhoto}
+            />
+            <h1 className={styles.title}>
+              Hello, I’m Mikhail, Junior Front-end Developer.
+            </h1>
+          </div>
           <button className={styles.descriptionButton}>
             skills I have{" "}
             <span className={styles.arrow}>
@@ -21,9 +30,7 @@ export const AboutMe: React.FC = () => {
             </span>
           </button>
         </div>
-        <div className={styles.boxPhoto}>
-          <img src={MyPhoto} alt="MyPhoto" />
-        </div>
+        <img className={styles.descriptionPhoto} src={MyPhoto} alt="MyPhoto" />
       </div>
     </div>
   );
--- src/components/HomeContent/AboutSite/AboutSite.tsx
+++ src/components/HomeContent/AboutSite/AboutSite.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./aboutSite.module.scss";
 import { ArrowRightOutlined } from "@ant-design/icons";
+import { motion } from "framer-motion";
 export const AboutSite = () => {
   return (
     <div className={styles.aboutSite}>
-      <div className={styles.logo}>
-        film<span className={styles.logoLetter}>X</span>
-      </div>
-      <div className={styles.description}>
+      <motion.div
+        initial={{ y: -500, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          ease: "easeInOut",
+          duration: 1,
+        }}
+        className={styles.logo}
+      >
+        film
+        <span className={styles.logoLetter}>X</span>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className={styles.description}
+      >
         <div className={styles.descriptionText}>
           If you, like me, live in Ukraine, this site only works with VPN .On
           this site you can view movie selections (top movies, popular movies),
@@ -17,15 +32,13 @@ export const AboutSite = () => {
           watched or liked movies, the data is stored in the Firebase Realtime
           DataBase.
         </div>
-        <div className={styles.button}>
-          <Link to={"/films"}>
-            GET STARTED
-            <span className={styles.arrow}>
-              <ArrowRightOutlined />
-            </span>
-          </Link>
-        </div>
-      </div>
+        <Link className={styles.button} to={"/films"}>
+          GET STARTED
+          <span className={styles.arrow}>
+            <ArrowRightOutlined />
+          </span>
+        </Link>
+      </motion.div>
     </div>
   );
 };
